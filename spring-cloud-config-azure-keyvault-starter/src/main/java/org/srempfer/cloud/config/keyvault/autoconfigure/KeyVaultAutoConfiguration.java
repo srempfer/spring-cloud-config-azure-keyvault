205368215c51bd715f7144ce5a777ac029eb9aee
Stefan Rempfer <rempfer@gmail.com>
2020-05-20 05:00:05 +0200

    Added possibility to use MSI / Managed identities to authenticate

--- spring-cloud-config-azure-keyvault-starter/src/main/java/org/srempfer/cloud/config/keyvault/autoconfigure/KeyVaultAutoConfiguration.java
+++ spring-cloud-config-azure-keyvault-starter/src/main/java/org/srempfer/cloud/config/keyvault/autoconfigure/KeyVaultAutoConfiguration.java
@@ -2,9 +2,14 @@ package org.srempfer.cloud.config.keyvault.autoconfigure;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.microsoft.azure.keyvault.spring.KeyVaultOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -23,14 +28,30 @@ import java.util.Collections;
 @Configuration
 public class KeyVaultAutoConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger ( KeyVaultAutoConfiguration.class );
+
     @ConditionalOnMissingBean
     @Bean
     public TokenCredential tokenCredential ( KeyVaultProperties properties ) {
-        return new ClientSecretCredentialBuilder ()
-            .clientId ( properties.getClientId () )
-            .clientSecret ( properties.getClientKey () )
-            .tenantId ( properties.getTenantId () )
-            .build ();
+        String clientId = properties.getClientId ();
+        String clientKey = properties.getClientKey ();
+
+        if ( StringUtils.isNoneBlank ( clientId, clientKey ) ) {
+            LOGGER.debug ( "Client secret credentials will be used" );
+            return new ClientSecretCredentialBuilder ()
+                .clientId ( clientId )
+                .clientSecret ( clientKey )
+                .tenantId ( properties.getTenantId () )
+                .build ();
+        }
+
+        if ( StringUtils.isNoneBlank ( clientId ) ) {
+            LOGGER.debug ( "MSI credentials with specified clientId will be used" );
+            return new ManagedIdentityCredentialBuilder ().clientId ( clientId ).build ();
+        }
+
+        LOGGER.debug ( "MSI credentials will be used" );
+        return new ManagedIdentityCredentialBuilder ().build ();
     }
 
     @ConditionalOnMissingBean
