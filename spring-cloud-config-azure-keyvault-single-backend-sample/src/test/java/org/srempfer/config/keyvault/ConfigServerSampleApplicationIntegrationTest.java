224aa96002880d76ca17cfc2b9c31db2b17f9e65
Stefan Rempfer <rempfer@gmail.com>
2020-11-05 19:08:30 +0100

    Changed to JUnit 5

--- spring-cloud-config-azure-keyvault-single-backend-sample/src/test/java/org/srempfer/config/keyvault/ConfigServerSampleApplicationIntegrationTest.java
+++ spring-cloud-config-azure-keyvault-single-backend-sample/src/test/java/org/srempfer/config/keyvault/ConfigServerSampleApplicationIntegrationTest.java
@@ -1,15 +1,12 @@
 package org.srempfer.config.keyvault;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -18,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith ( SpringRunner.class )
 @SpringBootTest ( classes = ConfigServerSampleApplication.class )
 @AutoConfigureMockMvc
 public class ConfigServerSampleApplicationIntegrationTest {
